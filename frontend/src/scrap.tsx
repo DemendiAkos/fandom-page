32b923ba1eac929273bd968d0ee6f71fab709881
RobozPeter <peti.roboz@gmail.com>
2024-05-22 10:07:48 +0200

    query fix

---
+++ frontend/src/scrap.tsx
@@ -0,0 +1,13 @@
+export type Scraps = Scrap[]
+
+
+
+export interface Scrap {
+    ScrapName: string
+    MinimumValue: number
+    Maximumvalue: number
+    Weight: number
+    Conductivity: number
+    IsTwoHanded: number
+  }
+  
